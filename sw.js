19b8241df57cf1fe055d71fa5c9f2e96f69d90a2
y-tachikawa-nandis <y-tachikawa@nandis.jp>
2021-12-01 15:22:42 +0900

    test

--- sw.js
+++ sw.js
@@ -1,3 +1,23 @@
+var CACHE_NAME = 'pwa-sample-cache';
+var urlsToCache = [
+    '/',
+    '/manifest.json',
+    '/css/style.css',
+    '/serviceworker.js',
+    '/count.js',
+];
+
+// インストール処理
+self.addEventListener('install', function(event) {
+    event.waitUntil(
+        caches
+            .open(CACHE_NAME)
+            .then(function(cache) {
+                return cache.addAll(urlsToCache);
+            })
+    );
+});
+
 self.addEventListener('fetch', function(e) {
   // ここは空でもOK
 })
